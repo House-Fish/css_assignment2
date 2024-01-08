f4b54b626aebba8e74699e2ef6b81c3d58cfe359
Brayden <S10258659@connect.np.edu.sg>
2024-01-08 09:34:28 +0800

    I imported mui only

---
+++ app/history/page.jsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@mui/material";
+
+function App() {
+    return (
+    <Container sx = {{bgcolor: "blue", height: "100vh"}}>
+        <Typography></Typography>
+    </Container>
+    );
+}
+
+export default App; 
