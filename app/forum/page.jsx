02a111ee6af9a3b5f9ff25de9c612d85ff3b3134
tev <tevelshorz@gmail.com>
2024-01-11 12:13:20 +0800

    Update

--- app/forum/page.jsx
+++ app/forum/page.jsx
@@ -1,3 +1,4 @@
+// ForumPage.jsx
 'use client';
 
 import '../globals.css';
@@ -6,42 +7,43 @@ import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
 
 const ForumPage = () => {
-    const [comment, setComment] = useState('');
-    const [comments, setComments] = useState([]);
-  
-    const handleCommentChange = (e) => {
-      setComment(e.target.value);
-    };
-  
-    const handleAddComment = () => {
-      if (comment.trim() !== '') {
-        setComments([...comments, comment]);
-        setComment('');
-      }
-    };
-  
-    return (
-      <div className={styles.forumPage}>
-        <h1>Flappy Bird Forum!</h1>
-  
-        <div className={styles.commentContainer}>
-          <textarea
-            placeholder="Write your comment..."
-            value={comment}
-            onChange={handleCommentChange}
-          ></textarea>
-          <button onClick={handleAddComment}>Comment</button>
-        </div>
-  
-        <div className={styles.comments}>
-          {comments.map((c, index) => (
-            <div key={index} className={styles.comment}>
-              {c}
-            </div>
-          ))}
-        </div>
-      </div>
-    );
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
+
+  const handleCommentChange = (e) => {
+    setComment(e.target.value);
   };
-  
-  export default ForumPage;
+
+  const handleAddComment = () => {
+    if (comment.trim() !== '') {
+      setComments([...comments, comment]);
+      setComment('');
+    }
+  };
+
+  return (
+    <div className={styles.forumPage}>
+      <h1>Flappy Bird Forum!</h1>
+
+      <div className={styles.commentContainer}>
+        <textarea
+          className={styles.textareaContainer}
+          placeholder="Write your comment..."
+          value={comment}
+          onChange={handleCommentChange}
+        ></textarea>
+        <button onClick={handleAddComment}>Comment</button>
+      </div>
+
+      <div className={styles.comments}>
+        {comments.map((c, index) => (
+          <div key={index} className={styles.comment}>
+            {c}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ForumPage;
