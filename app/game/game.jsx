599ca8b941c717cc4366e883b9a62ebf09876abe
LuKaito1412 <lu176844@gmail.com>
2024-01-15 13:17:22 +0800

    useEffect optimization

--- app/game/game.jsx
+++ app/game/game.jsx
@@ -2,7 +2,7 @@
 import './game.css';
 import React from "react";
 import { useState, useEffect } from 'react';
-
+import Instructions from '../instructions/page';
 
 // Things to add: moving background, changing obstacle height
 
@@ -21,104 +21,60 @@ function DeathPage({restartGame, score}) {
   );
 }
 
-function UpColliding(bird, upObstacle) {
-  const birdPos = bird.getBoundingClientRect();
-  const upObstaclePos = upObstacle.getBoundingClientRect();
-
+function UpColliding(birdPos, upObstaclePos) {
   // Needs to return false if alive, true if dead
   return (
-    birdPos.y <= upObstaclePos.height &&
-    birdPos.x + birdPos.width >= upObstaclePos.x &&
-    birdPos.x <= upObstaclePos.x + upObstaclePos.width
+    birdPos.top <= upObstaclePos.bottom &&
+    birdPos.right >= upObstaclePos.left &&
+    birdPos.left <= upObstaclePos.right
   );
 }
 
-function DownColliding(bird, downObstacle) {
-  const birdPos = bird.getBoundingClientRect();
-  const downObstaclePos = downObstacle.getBoundingClientRect();
-
+function DownColliding(birdPos, downObstaclePos) {
   // Needs to return false if alive, true if dead
   return (
-    birdPos.y + birdPos.height - downObstaclePos.y >= 0 &&
-    birdPos.x + birdPos.width >= downObstaclePos.x &&
-    birdPos.x <= downObstaclePos.x + downObstaclePos.width
+    birdPos.bottom >= downObstaclePos.top &&
+    birdPos.right >= downObstaclePos.left &&
+    birdPos.left <= downObstaclePos.right
   );
 }
 
 function Game({over, score, setScore}) {
   const [topDist, setTopDist] = useState(320);
-  const [downLeftDist, setDownLeftDist] = useState(540);
-  const [upLeftDist, setUpLeftDist] = useState(540);
+  const [leftDist, setLeftDist] = useState(540);
   const [gameStart, setGameStart] = useState(false);
 
   useEffect(() => {
     const collisionInterval = setInterval(() => {
-      const bird = document.querySelector(".bird");
-      const upObstacle = document.querySelector(".upObstacle");
-      const downObstacle = document.querySelector(".downObstacle")
+      if (gameStart == true) {
+      const birdPos = document.querySelector(".bird").getBoundingClientRect();
+      const upObstaclePos = document.querySelector(".upObstacle").getBoundingClientRect();
+      const downObstaclePos = document.querySelector(".downObstacle").getBoundingClientRect();
 
-      if ((topDist > 613) || (UpColliding(bird, upObstacle)) || (DownColliding(bird, downObstacle))) {
+      if ((topDist > 613) || (DownColliding(birdPos, downObstaclePos)) || (UpColliding(birdPos, upObstaclePos))) {
         setGameStart(false);
         over();
-    }}, 100);
+      }
+    }
+    });
 
       return () => clearInterval(collisionInterval);
-    }, [topDist]);
+    }, [topDist, leftDist]);
 
   useEffect(() => {
     const moveInterval = setInterval(() => {
-      if (gameStart) {
+      if (gameStart == true) {
         setScore((prevScore) => prevScore + 1);
-        if (topDist < 613) {
-          setTopDist((prevTopDist) => prevTopDist + 10.3);
-        }
-        setDownLeftDist((prevDownLeftDist) => prevDownLeftDist - 10);
-        setUpLeftDist((prevUpLeftDist) => prevUpLeftDist - 10);
-        if (downLeftDist < -160 || upLeftDist < -160) {
-          setDownLeftDist(550);
-          setUpLeftDist(550);
+        setTopDist((prevTopDist) => prevTopDist + 11);
+        setLeftDist((prevLeftDist) => prevLeftDist - 12);
+        if (leftDist < -160) {
+          setLeftDist(550);
         }
       }
-    }, 100);
+    }, 170);
 
       return () => clearInterval(moveInterval);
-    }, [gameStart, score, setScore, topDist, downLeftDist, upLeftDist]);
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     if (gameStart == true) {
-  //       setScore(score => score + 1);
-  //     }
-  //   }, 100);
-  
-  //   return () => clearInterval(interval);
-  // }, [gameStart]);
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     if (gameStart == true) {
-  //       if (topDist < 613) {
-  //         setTopDist(prevTopDist => prevTopDist + 10.3);
-  //       }
-  //     }
-  //   }, 100);
-  
-  //   return () => clearInterval(interval);
-  // }, [gameStart, topDist]);
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     if (gameStart == true) {
-  //         setDownLeftDist(prevDownLeftDist => prevDownLeftDist - 10);
-  //         setUpLeftDist(prevUpLeftDist => prevUpLeftDist - 10);
-  //       if ((downLeftDist < -160) || (upLeftDist < -160)) {
-  //         setDownLeftDist(550);
-  //         setUpLeftDist(550);
-  //       }
-  //     }}, 100);
-    
-  //   return () => clearInterval(interval);
-  // }, [gameStart, upLeftDist, downLeftDist]);
+    }, [gameStart, leftDist]);
 
   const handleClick = () => {
     if (gameStart == true)
@@ -135,9 +91,9 @@ function Game({over, score, setScore}) {
         <button className="startBtn" onClick={handleStart}>Start Game</button>
       )}
       <div className="score">{score}</div>
-      <img className="upObstacle" src="/upBlock.png" style={{marginLeft: upLeftDist}} />
+      <img className="upObstacle" src="/upBlock.png" style={{marginLeft: leftDist}} />
       <img className="bird" src="/sparrow.png" style={{top: topDist}} />
-      <img className="downObstacle" src="/downBlock.png" style={{marginLeft: downLeftDist}} />
+      <img className="downObstacle" src="/downBlock.png" style={{marginLeft: leftDist}} />
     </div>    
   )
 }
@@ -156,8 +112,7 @@ function App() {
   };
 
   return (
-    <div className='websiteContainer'>
-      <div className='gameWindow'>
+    <div className="gameWindow">
       {gameRun == true ? (
         <div>
           <Game over={handleOver} score={score} setScore={setScore} />
@@ -166,7 +121,6 @@ function App() {
       : (
         <DeathPage restartGame={handleRestart} score={score} />
       )}
-    </div>
     </div>
   );
 }
