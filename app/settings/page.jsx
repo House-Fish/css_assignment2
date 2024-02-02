bcbde5f53ae326f247d1cfee993279e7e9aa22db
HouseFish <leehousefish@gmail.com>
2024-02-02 17:34:00 +0800

    Inital settings page

--- app/settings/page.jsx
+++ app/settings/page.jsx
@@ -1,7 +1,24 @@
-import styles from "./page.module.css";
+"use client"
 
-function Settings() {
-    return <h1>Settings</h1>
-}
+import React from "react";
+import "./style.css";
+import Controls from "./controls";
+import Audio from "./audio";
+import Customize from "./customize";
 
-export default Settings;
+export default function Settings() {
+
+
+  return (
+    <div id="root">
+      <Controls name={'Jump'} defaultKey={'Space'}/>
+      <Controls name={'Pause'} defaultKey={'p'}/>
+      <Audio name={'Music'}/>
+      <Audio name={'Effects'}/>
+      <Audio name={'Master'}/> 
+      <Customize name="Bird"/>
+      <Customize name="Obstacles"/>
+      <Customize name="Background"/>
+    </div>
+  );
+};
